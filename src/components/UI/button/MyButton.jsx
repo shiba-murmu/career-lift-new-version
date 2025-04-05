10ca94026b87d6b5ce66065b0c454caf6b746a6f
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-06 00:24:56 +0530

    Some changes in the background of buttons and removed className.

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 // import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-
+// Add an icon to click the button..
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+import { grey } from "@mui/material/colors";
 /**
  *
  * @param {import("@mui/material").ButtonProps} param0
@@ -10,11 +12,21 @@ import Button from "@mui/material/Button";
  * This is the button components here to be used in the whole website
  */
 
-const MyButton = (props) => {
+const MyButton = ({ children }) => {
   return (
     <>
-      <Button variant="contained" className={props.className}>
-        {props.children}
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          backgroundColor: grey[900],
+          "&:hover": {
+            backgroundColor: grey[700],
+          },
+        }}
+        endIcon={<AdsClickIcon />}
+      >
+        {children}
       </Button>
     </>
   );
