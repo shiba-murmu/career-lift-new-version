15662e1f5da39e3313c2ff2c7f947df5674188f7
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-06 01:48:28 +0530

    some changes here.

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 const darkTheme = createTheme({
   palette: {
-    mode: "light",
+    mode: "dark",
   },
 });
 // ****************************************************************
