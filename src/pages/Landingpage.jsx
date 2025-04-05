162cae0aba49f73fb4e19afbdad43774582d2a34
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-05 22:26:45 +0530

    Make changes in the landingpage.jsx file to set the font family as ' Open Sans'.

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -14,7 +14,7 @@ function Landingpage() {
         <div className="overlay"></div>
         {/* Content */}
         <div className="content">
-          <h1 className="text-5xl md:text-9xl font-extrabold">Career Lift</h1>
+          <h1 className="text-5xl md:text-9xl font-extrabold"  style={{fontFamily: "'Open Sans', sans-serif", fontWeight: "bold"}}>Career Lift</h1>
           <p className="text-lg md:text-2xl">Explore opportunities to elevate your career with Career Lift</p>
           <Link to="/home" className="bg-blue-500 ">
             <MyButton className="bg-blue-500 text-white font-bold py-2  px-4 rounded-full hover:bg-blue-700 transition duration-300">Get Started</MyButton>
