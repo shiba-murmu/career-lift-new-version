0ba735cfaf57bf485790c656815356f38d4489ea
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-06 13:38:00 +0530

    One comment and one more function is added here in this file.

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -3,6 +3,11 @@ import Transition from "../../components/sliderEffect/Transition";
 import "./home.css";
 import BackgroundParticles from "../../components/UI/particle/BackgroundParticles";
 function SectionTwo() {
+  /**
+   *  The SectionThree() component is a functional component that returns a JSX element.
+   *  It is a header component that is used to display the header of the page.
+   *  @returns a JSX element..
+   */
   return (
     <>
       <div className=" min-h-screen w-full flex justify-center flex-col items-center" style={{ position: "relative", overflow: "hidden" }}>
@@ -56,9 +61,49 @@ function SectionTwo() {
 }
 
 function SectionThree() {
+  /**
+   * Next working for this page is to design here..
+   * Pending workings here.
+   */
   return (
+    
     <>
       {/* <SectionThree /> */}
+      <div className="pl-10 min-h-screen w-full flex justify-start flex-col items-start">
+        <div className="text-start relative">
+          <h1
+            className="text-2xl md:text-6xl font-bold"
+            style={{
+              fontFamily: "'Open Sans', sans-serif",
+              fontWeight: "bolder",
+            }}
+          >
+            Build your <span className="highlight">career</span>
+          </h1>
+        </div>
+
+        <div className="text-start relative">
+          <p
+            className="text-sm md:text-2xl text-neutral-400"
+            style={{
+              fontFamily: "'Raleway', sans-serif",
+              fontWeight: "SemiBold",
+            }}
+          >
+            Career lift is the platform where you can explore a wide range of
+          </p>
+          <p
+            className="text-sm md:text-2xl text-neutral-400"
+            style={{
+              fontFamily: "'Raleway', sans-serif",
+              fontWeight: "SemiBold",
+            }}
+          >
+            opportunities to elevate your career with the
+            <b className="highlight">help of AI assistant.</b>
+          </p>
+        </div>
+      </div>
     </>
   )
 }
