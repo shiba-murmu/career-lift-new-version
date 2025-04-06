261d69384af52b19e279bcaf6ea21aa2b3da1f34
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-06 10:00:11 +0530

    changes homepage design and tsparticles library installed.

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Transition from "../../components/sliderEffect/Transition";
-// import "./home.css";
-import './home.css';
+import "./home.css";
 
 function SectionTwo() {
   return (
     <>
-      <div className="h-96 w-full bgGradient flex items-center">
-        <div className="pl-14">
+      <div className="min-h-screen w-full flex justify-center flex-col items-center py-10">
+        <div className="text-center">
           <h1
             className="text-2xl md:text-6xl font-bold"
             style={{
@@ -27,6 +26,16 @@ function SectionTwo() {
           >
             to elevate your <span className="highlight">career</span>
           </h1>
+        </div>
+        <br />
+        <div className="text-center">
+          <p className="text-lg md:text-2xl text-neutral-400" style={{fontFamily: "'Raleway', sans-serif", fontWeight: "SemiBold"}}>
+            Career lift is the platform where you can explore a wide range of
+          </p>
+          <p className="text-lg md:text-2xl text-neutral-400" style={{fontFamily: "'Raleway', sans-serif", fontWeight: "SemiBold"}}>
+            opportunities to elevate your career with the
+            <b className="highlight">help of AI assistant.</b>
+          </p>
         </div>
       </div>
     </>
