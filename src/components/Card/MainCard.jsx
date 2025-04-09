1f270e9bfac3dd29b3b8113326d6c1a8570b5a0e
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-10 02:03:11 +0530

    changes made in the cards component.

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -10,22 +10,34 @@ const MainCard = () => {
         </div> */}
         <div className="card-content">
           <h2
-            className="card-title text-xl md:text-2xl text-center"
+            className="card-title text-xl p-5 pb-0 md:text-2xl text-center"
             style={{ fontFamily: "Open Sans, sans-serif", fontWeight: "bold" }}
           >
             CAREER AFTER GRADUATION
           </h2>
           <p
-            className="card-description text-sm md:text-lg"
+            className="card-description p-10 pt-5 text-sm md:text-lg"
             style={{ fontFamily: "Raleway, sans-serif" }}
           >
-            Explore careers is a platform where you can explore different
-            careers and find the one that best suits you.
+            Career lift is designed to help graduates make informed decisions
+            about their future. With an array of career paths and resources,
+            this platform empowers individuals to choose a path that aligns with
+            their skills and aspirations, ensuring a fulfilling and successful
+            career after graduation. Whether you are looking to pursue a career
+            in a traditional industry or explore emerging fields, Career lift
+            provides access to valuable insights and tools to help you navigate
+            the ever-changing job market. From resume building to networking,
+            our platform offers a comprehensive suite of resources to support
+            your professional development. By leveraging our expertise and
+            guidance, you can confidently embark on a career path that aligns
+            with your goals and values.
           </p>
           <div className="text-center">
             <Button
               variant="contained"
-              style={{ fontFamily: "Raleway, sans-serif", fontWeight: "bold" }}
+              size="large"
+              color="secondary"
+              style={{ fontFamily: "Raleway, sans-serif", fontWeight: "bold", marginBottom : "5px" }}
             >
               Explore
             </Button>
