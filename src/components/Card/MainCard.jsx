0aed13637588472360e6e0bb400e2fa71102885e
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-10 01:52:53 +0530

    changes made adjust the fonts.

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -4,19 +4,19 @@ import Button from "@mui/material/Button";
 const MainCard = () => {
   return (
     <>
-      <div className="md:w-[50%] md:h-[50%] bg-amber-400 rounded-xl p-3">
+      <div className="md:w-[50%] md:h-[50%] bg-gray-200 shadow-2xl rounded-xl p-3">
         {/* <div className="card-image">
           <img src="https://picsum.photos/200/300" alt="card image" />
         </div> */}
         <div className="card-content">
           <h2
-            className="card-title text-3xl text-center"
+            className="card-title text-xl md:text-2xl text-center"
             style={{ fontFamily: "Open Sans, sans-serif", fontWeight: "bold" }}
           >
-            Career After 10th
+            CAREER AFTER GRADUATION
           </h2>
           <p
-            className="card-description text-lg"
+            className="card-description text-sm md:text-lg"
             style={{ fontFamily: "Raleway, sans-serif" }}
           >
             Explore careers is a platform where you can explore different
