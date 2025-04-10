f4f26a5ae81c1608336c06bae24e345a814ead14
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-10 23:28:37 +0530

    props passing to the Background component and images path has been added and passing it to that component.

--- src/components/BackgroundImage/BgFirstImage.jsx
+++ src/components/BackgroundImage/BgFirstImage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 
-function Background() {
+function Background({bgImage , stream}) {
     /**
      * Actual background model is here...
      */
     return(
         <>
             <div className='w-full relative'>
-               <img src="./images/career.jpeg" alt="background"
+               <img src={bgImage} alt="background"
                className='w-full h-50 md:h-50 object-center object-fit object-cover' />
                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                  <h1 className='text-3xl md:text-6xl text-white' style={{ fontFamily: "'Open Sans', sans-serif", fontWeight: "bolder" }}>INTERMEDIATE</h1>
+                  <h1 className='text-3xl md:text-6xl text-white' style={{ fontFamily: "'Open Sans', sans-serif", fontWeight: "bolder" }}>{stream}</h1>
                </div>
             </div>
         </>
@@ -25,19 +25,19 @@ function BgFirstImage({ whichPage }) {
         case 'matriculation':
             return (
                 <>
-                    <Background />
+                    <Background stream={"Matriculation"} bgImage={"https://images.pexels.com/photos/29139964/pexels-photo-29139964/free-photo-of-vibrant-neon-abstract-light-streaks.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}/>
                 </>
             )
         case 'intermediate':
             return (
                 <>
-                   <Background />
+                   <Background stream={"Intermediate"} bgImage={"https://images.pexels.com/photos/29586678/pexels-photo-29586678/free-photo-of-abstract-blue-digital-art-landscape-for-desktop.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} />
                 </>
             )
         case 'graduation':
             return (
                 <>
-                    <Background />
+                    <Background stream={"Graduation"} bgImage={"https://images.pexels.com/photos/29764258/pexels-photo-29764258/free-photo-of-monochromatic-abstract-sphere-on-rippling-surface.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} />
                 </>
             )
         default:
