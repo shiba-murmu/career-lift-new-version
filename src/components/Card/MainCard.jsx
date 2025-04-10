c88341670d3a52ee66ddb4bb7cf2ab69344405ce
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-10 19:51:54 +0530

    One comment is added in the file ExploreCareer.jsc and Link tags in opened in the component for MainCard.jsx

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "@mui/material/Button";
-
+import { Link } from "react-router-dom";
 function MainCard({ heading, description }) {
   return (
     <>
-      <div className="sm:w-3xl  md:w-5xl bg-gray-600 shadow-xl p-3">
+      <div className="sm:w-3xl  md:w-5xl bg-gray-200 shadow-lg p-3">
         <div className="card-content">
           <h2
-            className="card-title text-xl p-5 pb-0 md:text-2xl text-center"
+            className="card-title text-blue-600 text-xl p-5 pb-0 md:text-3xl text-center"
             style={{ fontFamily: "Open Sans, sans-serif", fontWeight: "bold" }}
           >
             {heading}
@@ -19,19 +19,21 @@ function MainCard({ heading, description }) {
             {description}
           </p>
           <div className="text-center">
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              className="w-40 md:w-50"
-              style={{
-                fontFamily: "Raleway, sans-serif",
-                fontWeight: "bold",
-                marginBottom: "2rem",
-              }}
-            >
-              Explore
-            </Button>
+            <Link to="/explore">
+              <Button
+                variant="contained"
+                size="large"
+                color="secondary"
+                className="w-40 md:w-50"
+                style={{
+                  fontFamily: "Raleway, sans-serif",
+                  fontWeight: "bold",
+                  marginBottom: "2rem",
+                }}
+              >
+                Explore
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
