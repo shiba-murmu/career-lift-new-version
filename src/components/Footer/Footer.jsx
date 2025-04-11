cf89cf75b7a9e0d2c36dda174750818afc93e9d3
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-11 20:57:40 +0530

    Footer is set to relative..

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -170,7 +170,7 @@ function Footer() {
   return (
     <>
       {/* <!-- Footer container --> */}
-      <footer className="bg-zinc-50 text-center text-surface/75 dark:bg-blue-900 dark:text-white/75 lg:text-left">
+      <footer className="bg-zinc-50 relative text-center text-surface/75 dark:bg-blue-900 dark:text-white/75 lg:text-left">
         <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
           <div className="me-12 hidden lg:block">
             <span>Get connected with us on social networks:</span>
