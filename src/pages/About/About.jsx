679b41ca5451a2934bc527d46e4321dac9eb8fd7
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-12 18:02:09 +0530

    image background added to the about.jsx page here.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,17 +12,9 @@ function AboutUSheading() {
 function SectionOne() {
   return(
     <>
-      <div className="felx-col md:flex md:h-[90vh] bg-gray-900">
-        <div className="w-1/2 p-50 grid place-content-center">
-          <img src="https://images.pexels.com/photos/9783350/pexels-photo-9783350.jpeg?auto=compress&cs=tinysrgb&w=600" alt="ai image" 
-          className="w-full rounded-lg"/>
-        </div>
-        <div className="w-1/2 flex flex-col justify-center items-start">
-        <h2 className="text-4xl font-bold mb-4 text-white">Keep it simple</h2>
-          <p className="pr-30 text-lg text-white">
-          CareerLift is a company that specializes in helping individuals navigate their career journeys. Our team of experts has years of experience in the field of career development and has designed this website with the purpose of providing a comprehensive resource for individuals looking to advance their careers. We believe that everyone deserves to have a fulfilling career and we are committed to helping individuals achieve their career goals. This website is designed to provide a user-friendly interface that allows individuals to easily navigate and find the resources they need. Whether you are just starting out in your career or are looking to make a change, we have the tools and resources you need to succeed. Our team is dedicated to providing the best possible experience for our users and we are constantly working to improve and expand our website. We hope that you find this website to be a valuable resource in your career journey.
-          </p>
-        </div>
+      <div>
+        <img src="https://images.pexels.com/photos/28669382/pexels-photo-28669382/free-photo-of-modern-skyscraper-architecture-in-cityscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="High buildings"
+        className="w-full h-screen md:h-screen object-center object-fit object-cover imgBackground" />
       </div>
     </>
   )
@@ -39,4 +31,10 @@ function About() {
   )
 }
 
-export default About
+export default About;
+
+
+// <div className="w-1/2 p-50 grid place-content-center">
+//   <img src="https://images.pexels.com/photos/9783350/pexels-photo-9783350.jpeg?auto=compress&cs=tinysrgb&w=600" alt="ai image" 
+//   className="w-full rounded-lg"/>
+// </div>
