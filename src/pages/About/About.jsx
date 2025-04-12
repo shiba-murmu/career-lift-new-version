54f421fdfe00ba2e57fb7832ebbff396e117dd23
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-12 21:46:00 +0530

    one about.css is deleted here..

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './About.css'
+
 function About() {
   /**
-   * Main function of the page here..
+   * Main function of the page here...
    */
   return (
     <div>About</div>
   )
 }
 
-export default About;
+export default About
