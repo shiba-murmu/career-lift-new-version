c537e86533b8ca1995b16592f90fb48479f2e18b
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-13 22:07:09 +0530

    buttons options changed here.

--- src/pages/Explorecareer/ButtonOptions.jsx
+++ src/pages/Explorecareer/ButtonOptions.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Button from 
+// import Button from
 function ButtonOptions() {
   /**
    * These buttons collection is commons to all the courses
@@ -8,15 +8,21 @@ function ButtonOptions() {
    */
   return (
     <>
-      <div className="flex flex-wrap p-5 justify-center  text-white text-center">
-        <div className="flex-1/3 bg-amber-300">
-            Higher studies
+      <div className="block md:flex flex-wrap p-2 justify-center  text-white text-center bg-gradient-to-b from-gray-900 to-gray-600">
+        <div className="flex-1/3 border-b-2 md:border-b-0">
+          <button className="bg-gray-500 w-full hover:bg-gray-700 md:hover:scale-105 hover:scale-105 transition duration-300 text-white md:font-medium font-light text-md md:text-lg  rounded-0 py-2">
+            EXPLORE HIGHER COURSES
+          </button>
         </div>
-        <div className="flex-1/3 bg-amber-950">
-            Jobs
+        <div className="flex-1/3 border-b-2 md:border-b-0">
+          <button className="bg-gray-500 w-full hover:bg-gray-700 md:hover:scale-105 hover:scale-105 transition duration-300 text-white md:font-medium font-light text-md md:text-lg  rounded-0 py-2">
+            EXPLORE JOBS
+          </button>
         </div>
-        <div className="flex-1/3 bg-blue-950">
-            
+        <div className="flex-1/3">
+          <button className="bg-gray-500 w-full hover:bg-gray-700 md:hover:scale-105 hover:scale-105 transition duration-300 text-white md:font-medium font-light text-md md:text-lg  rounded-0 py-2">
+            EXPLORE COMPETITIVE EXAMS
+          </button>
         </div>
       </div>
     </>
