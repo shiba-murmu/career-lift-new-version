bc77c68fe2d96bcc5b96079748d0a308bcdc5a7d
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-13 18:15:43 +0530

    one model is ceated here.

---
+++ src/pages/Explorecareer/ButtonOptions.jsx
@@ -0,0 +1,14 @@
+import React  from "react";
+
+function ButtonOptions() {
+    /**
+     * These buttons collection is commons to all 
+     * And these button will change the interface 
+     * according to options clicked
+     */
+    return (
+        <>
+            
+        </>
+    )
+}
