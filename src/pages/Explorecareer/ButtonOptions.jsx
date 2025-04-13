c96877bc6170c50151cc25b502c3680a3181889a
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-13 18:22:40 +0530

    One model is created and these model is help to maintain the ui for all the courses by using just buttons clicked options.

--- src/pages/Explorecareer/ButtonOptions.jsx
+++ src/pages/Explorecareer/ButtonOptions.jsx
@@ -2,13 +2,13 @@ import React  from "react";
 
 function ButtonOptions() {
     /**
-     * These buttons collection is commons to all 
+     * These buttons collection is commons to all the courses
      * And these button will change the interface 
      * according to options clicked
      */
     return (
         <>
-            
+
         </>
     )
 }
