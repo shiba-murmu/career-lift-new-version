6576af83bbe2b819ca68536ca0a7d9a1608f0b0b
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-15 18:57:24 +0530

    some commented code is removed from the file.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { GetMuiTheme } from "./components/MUItheme/GetMuiTheme.js";
-// import { UseTailwindThemeMode } from "./components/hooks/UseTailWindThemeMode.js";
-// import { UseTailWindThemeMode } from "./components/hooks/useTailwindThemeMode.js";
 import { TailwindThemeMode } from "./components/hooks/TailwindThemeMode.js";
 
 /**
